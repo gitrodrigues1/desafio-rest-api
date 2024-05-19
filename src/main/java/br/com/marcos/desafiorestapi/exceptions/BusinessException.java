2610064adea1207549fdfd731ef9e4c793586b66
Marcos Ramos <rodrigues.ramos@gmail.com>
2024-05-19 19:32:50 -0300

    add exception handlers

---
+++ src/main/java/br/com/marcos/desafiorestapi/exceptions/BusinessException.java
@@ -0,0 +1,34 @@
+package br.com.marcos.desafiorestapi.exceptions;
+
+public class BusinessException {
+
+    public static class CustomerAlreadyExistsException extends RuntimeException {
+        public CustomerAlreadyExistsException() {
+            super("Customer already exists.");
+        }
+    }
+
+    public static class CustomerNotFoundException extends RuntimeException {
+        public CustomerNotFoundException() {
+            super("Customer not found.");
+        }
+    }
+
+    public static class CityAlreadyExistsException extends RuntimeException {
+        public CityAlreadyExistsException() {
+            super("City already exists.");
+        }
+    }
+
+    public static class InvalidCityIdException extends RuntimeException {
+        public InvalidCityIdException() {
+            super("City id not found.");
+        }
+    }
+
+    public static class InvalidYearOrAgeException extends RuntimeException {
+        public InvalidYearOrAgeException() {super("The age informed is not corresponding with birth date.");}
+    }
+
+
+}

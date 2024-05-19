1e7832daf8d70c0cc791052191c25ae3a94da4a8
Marcos Ramos <rodrigues.ramos@gmail.com>
2024-05-19 19:03:03 -0300

    Initial commit

---
+++ src/test/java/br/com/marcos/desafiorestapi/DesafioRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcos.desafiorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

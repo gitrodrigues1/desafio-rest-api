c93fafece4cf68d5fc7e18650a13d5e480985752
Marcos Ramos <rodrigues.ramos@gmail.com>
2024-05-19 19:12:28 -0300

    add state domain and repository

---
+++ src/main/java/br/com/marcos/desafiorestapi/domain/State.java
@@ -0,0 +1,23 @@
+package br.com.marcos.desafiorestapi.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class State {
+
+    @Id
+    private String id;
+    private String name;
+
+
+
+}

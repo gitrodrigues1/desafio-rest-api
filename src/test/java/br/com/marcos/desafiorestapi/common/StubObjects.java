25ae448bf40d2b37451bdf2cc9dd2599e5a725fc
Marcos Ramos <rodrigues.ramos@gmail.com>
2024-05-21 10:05:32 -0300

    test: add city service tests

--- src/test/java/br/com/marcos/desafiorestapi/common/StubObjects.java
+++ src/test/java/br/com/marcos/desafiorestapi/common/StubObjects.java
@@ -4,9 +4,17 @@ import br.com.marcos.desafiorestapi.domain.City;
 import br.com.marcos.desafiorestapi.domain.State;
 import br.com.marcos.desafiorestapi.dtos.CreateCityRequest;
 
+import java.util.List;
+
 public class StubObjects {
 
     public static final CreateCityRequest REQUEST = new CreateCityRequest("São Paulo", "SP");
-    public static final City CITY = new City("São Paulo", new State("SP", "São Paulo"));
+    public static final City CITY = new City(1L, "Fortaleza", new State("CE", "Fortaleza"));
+    public static final String UNKNOWN_CITY_NAME = "Jaboatão dos Guararapes";
     public static final State STATE = new State("SP", "São Paulo");
+
+    public static final List<City> CITIES = List.of(
+            new City(1L, "Olinda", new State("PE", "Pernambuco")),
+            new City(2L, "Recife", new State("PE", "Pernambuco"))
+    );
 }
--- src/test/java/br/com/marcos/desafiorestapi/services/impl/CityServiceTest.java
+++ src/test/java/br/com/marcos/desafiorestapi/services/impl/CityServiceTest.java
@@ -1,55 +1,110 @@
 package br.com.marcos.desafiorestapi.services.impl;
 
-import br.com.marcos.desafiorestapi.common.StubObjects;
 import br.com.marcos.desafiorestapi.domain.City;
 import br.com.marcos.desafiorestapi.domain.State;
 import br.com.marcos.desafiorestapi.dtos.CreateCityRequest;
-import br.com.marcos.desafiorestapi.dtos.CreateCityResponse;
+import br.com.marcos.desafiorestapi.dtos.CityResponse;
+import br.com.marcos.desafiorestapi.exceptions.BusinessException;
 import br.com.marcos.desafiorestapi.repositories.CityRepository;
 import br.com.marcos.desafiorestapi.repositories.StateRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.Mockito.times;
+import static br.com.marcos.desafiorestapi.common.StubObjects.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest(classes = CityService.class)
+
 class CityServiceTest {
 
     @InjectMocks
     private CityService cityService;
-
     @Mock
     private CityRepository cityRepository;
-
     @Mock
     private StateRepository stateRepository;
 
-    City city;
+    private City city;
+    private State state;
+    private CreateCityRequest createCityRequest;
 
     @BeforeEach
     void setup() {
         MockitoAnnotations.openMocks(this);
-        city = StubObjects.CITY;
+        state = new State("CE", "Ceará");
+        city = new City(1L, "Fortaleza", new State("CE", "Ceará"));
+        createCityRequest = new CreateCityRequest("Fortaleza", "CE");
     }
-
     @Test
     void createCity_WithValidData_shouldReturnCity() {
         var request = new CreateCityRequest("Fortaleza", "CE");
-        var response = new CreateCityResponse(1L, "Fortaleza", "CE");
 
-        Mockito.when(cityService.createCity(request)).thenReturn(response);
-        Mockito.when(cityRepository.save(city)).thenReturn(city);
-        Mockito.when(stateRepository.findById(request.stateId())).thenReturn(Optional.of(new State("CE", "Ceará")));
+        when(stateRepository.findById(Mockito.anyString())).thenReturn(Optional.of(state));
+        when(cityRepository.existsByNameAndStateId(createCityRequest.name(), createCityRequest.stateId())).thenReturn(false);
+        when(cityRepository.save(any())).thenReturn(city);
 
-        Mockito.verify(cityService, times(1)).createCity(request);
+        var response = cityService.createCity(request);
+
+        assertThat(response).isEqualTo(new CityResponse(CITY));
 
     }
+
+    @Test
+    void createCity_WithInvalidData_shouldThrowException() {
+
+        when(stateRepository.findById(Mockito.anyString()))
+                .thenReturn(Optional.of(state));
+        when(cityRepository.existsByNameAndStateId(createCityRequest.name(), createCityRequest.stateId()))
+                .thenReturn(true);
+
+        assertThatThrownBy(() -> {
+                    cityService.createCity(createCityRequest);
+                })
+                .isInstanceOf(BusinessException.CityAlreadyExistsException.class)
+                .hasMessage("City already exists.");
+    }
+
+    @Test
+    void findCity_WithValidName_ShouldReturnCitiesList() {
+        when(cityRepository.findCityByNameContainsIgnoreCase(city.getName()))
+                .thenReturn(Optional.of(java.util.List.of(city)));
+
+        var response = cityService.findCityByName(city.getName());
+
+        assertThat(response).isEqualTo(List.of(new CityResponse(CITY)));
+    }
+
+    @Test
+    void findCity_WithNonExistingName_ShouldReturnEmptyList() {
+        when(cityRepository.findCityByNameContainsIgnoreCase(city.getName()))
+                .thenReturn(Optional.of(Collections.emptyList()));
+
+        var response = cityService.findCityByName(UNKNOWN_CITY_NAME);
+
+        assertThat(response).isEqualTo(Collections.emptyList());
+    }
+
+    @Test
+    void findCity_WithExistingState_ShouldReturnCitiesList() {
+        when(cityRepository.findCityByState(CITIES.get(0).getState().getId()))
+                .thenReturn(Optional.of(CITIES));
+
+        var response = cityService.findCityByState(CITIES.get(0).getState().getId());
+
+        assertThat(response.get(0).stateId()).isEqualTo("PE");
+    }
+
+
 
 }
